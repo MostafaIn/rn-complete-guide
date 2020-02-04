da2cb9aee1bf09d735671dbff8c18e670bfa8cd3
mostafaIn <mostafa2015se@gmail.com>
2020-02-04 22:14:25 +0200

    21.getting started with styles

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding:50}}>
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+        <TextInput 
+          placeholder="Course Goal"
+          style={{width:'80%', borderWidth:1,borderStyle:'solid',borderColor:'#001', padding:10}}
+        />
+        <Button title="ADD" />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
