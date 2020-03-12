20384c7db530cd9c30ab2da038da75d4ed6eefb3
mostafaIn <mostafa2015se@gmail.com>
2020-03-12 13:55:53 +0200

    22 - 28 working with State & Events, ScrollView! and list of items

--- App.js
+++ App.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button } from 'react-native';
 
-export default function App() {
+const App = () =>{
+  const [enteredGoal,setEnteredGoal] = useState('')
+  const [courseGoals,setCourseGoals] = useState([])
+
+  const handleAdd = () =>{
+    setCourseGoals( courseGoals => [...courseGoals,enteredGoal])
+  }
   return (
-    <View style={{padding:50}}>
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+    <View style={styles.container}>
+      <View style={styles.controller}>
         <TextInput 
           placeholder="Course Goal"
-          style={{width:'80%', borderWidth:1,borderStyle:'solid',borderColor:'#001', padding:10}}
+          style={styles.input}
+          value={enteredGoal}
+          onChangeText={enteredGoal => setEnteredGoal(enteredGoal)}
         />
-        <Button title="ADD" />
+        <Button 
+        title="ADD" 
+        style={styles.btn}
+        onPress={handleAdd}
+        />
       </View>
+      <ScrollView>
+        {courseGoals.map( (goal, index) => <View key={index + 1}>
+          <Text style={styles.item}>{index + 1}.{goal}</Text>
+        </View>)}
+      </ScrollView>
     </View>
   );
-}
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    padding: 40
+  },
+  controller:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    paddingVertical:40
+  },
+  input:{
+    width:'80%',
+    borderWidth:1,
+    borderStyle:'solid',
+    borderColor:'#001',
+    borderRadius:25, 
+    padding:10
+  },
+  btn:{
+    borderWidth:1,
+    borderColor:'#333',
+    borderStyle:'solid',
+    borderRadius:50,
+    padding: 5,
+    backgroundColor:'#333'
+  },
+  item:{
+    padding:10,
+   
+  }
+})
